0b7b29c2013e38ec5c567b6750df82ae2b9e91ae
mateuszw.tech <mateuszw.tech@gmail.com>
2024-06-28 12:35:44 +0200

    add scraper code

---
+++ fetcher.py
@@ -0,0 +1,111 @@
+import concurrent
+import time
+from abc import ABC, abstractmethod
+from concurrent.futures import ThreadPoolExecutor
+from typing import List
+
+from bs4 import BeautifulSoup
+import asyncio
+import aiohttp
+
+from utils import SprzedajemyUtils, ScraperUtils
+
+
+class AdvertisementInfo:
+
+    def __init__(
+            self,
+            title: str,
+            username: str,
+            location: str,
+            phone_number: str,
+            price: str,
+            url: str,
+    ):
+        self.title = title
+        self.username = username
+        self.location = location
+        self.phone_number = phone_number
+        self.price = price
+        self.url = url
+
+    def display_information(self):
+        print(
+            "Title: "
+            + self.title
+            + "\nUsername: "
+            + self.username
+            + "\nLocation: "
+            + self.location
+            + "\nPhone Number: "
+            + self.phone_number
+            + "\nPrice: "
+            + self.price
+            + "\nURL: "
+            + self.url
+        )
+
+
+class Fetcher(ABC):
+    @abstractmethod
+    def load_osint_data_async(self, offer_list) -> List[AdvertisementInfo]:
+        raise NotImplementedError()
+
+
+class SprzedajemyFetcher(Fetcher):
+
+    def __init__(self, *cities):
+        self.cities = cities
+        self.current_data: list[AdvertisementInfo] = []
+
+    def get_all_offers_urls(self) -> List[str]:
+        pages = ScraperUtils.get_all_pages_urls_from_different_cities(SprzedajemyUtils.get_all_pages_urls, *self.cities)
+        offer_urls_futures = []
+        results_list = []
+        with ThreadPoolExecutor(max_workers=16) as executor:
+
+            for page in pages:
+                offer_urls_futures.append(executor.submit(SprzedajemyUtils.find_all_offers_in_selected_page, page))
+
+            completed_futures, _ = concurrent.futures.wait(offer_urls_futures)
+            for future in completed_futures:
+                results_list.append(future.result())
+
+            offers = [result for results in results_list for result in results]
+
+        return offers
+
+    def change_cities(self, *cities) -> None:
+        self.cities = cities
+
+    async def append_data(self, info: AdvertisementInfo) -> None:
+        self.current_data.append(info)
+
+    async def load_data_async(self, offer_url):
+        try:
+            async with aiohttp.ClientSession(trust_env=True, timeout=aiohttp.ClientTimeout(total=20)) as session:
+                async with session.get(offer_url, timeout=25) as resp:
+                    body = await resp.text()
+                    soup = BeautifulSoup(body, 'html.parser')
+                    title = SprzedajemyUtils.get_offer_title(soup)
+                    username = SprzedajemyUtils.get_offer_username(soup)
+                    location = SprzedajemyUtils.get_offer_location(soup)
+                    phone_number = SprzedajemyUtils.get_offer_phone_number(soup)
+                    price = SprzedajemyUtils.get_offer_price(soup)
+                    url = offer_url
+                    print("test " + offer_url)
+            await self.append_data(AdvertisementInfo(title, username, location, phone_number, price, url))
+        except Exception as e:
+            print({e})
+            raise Exception
+
+    async def load_osint_data_async(self, offer_list: list[str]) -> None:
+        tasks = []
+        for offer in offer_list:
+            task = asyncio.create_task(ScraperUtils.fail_repeat_execution(self.load_data_async, offer))
+            tasks.append(task)
+
+        await asyncio.gather(*tasks)
+
+    def get_osint_data(self):
+        return self.current_data
---
+++ utils.py
@@ -0,0 +1,136 @@
+import concurrent.futures
+import time
+import asyncio
+
+import requests
+from bs4 import BeautifulSoup
+from tqdm import tqdm
+
+from concurrent.futures import ThreadPoolExecutor
+
+
+class ScraperUtils:
+
+    @staticmethod
+    def return_website_string_as_bs4_content(website: str):
+        content = requests.get(website)
+        return BeautifulSoup(content.content, "html.parser")
+
+    @staticmethod
+    def replace_polish_characters(input_text: str) -> str:
+        strange = "ą, ć, ę, ł, ń, ó, ś, ź, ż"
+
+        ascii_replacements = "a, c, e, l, n, o, s, z, z"
+
+        translator = str.maketrans(strange, ascii_replacements)
+
+        return input_text.translate(translator)
+
+    # todo: Add Threading
+    @staticmethod
+    def get_all_pages_urls_from_different_cities(pages_scraping_method, *args):
+        pages = [result for arg in args for result in pages_scraping_method(arg)]
+        return pages
+
+    @staticmethod
+    async def fail_repeat_execution(function: callable, *args, max_retries: int = 10, sleep_time: float = 1):
+        retry_count = 0
+        while retry_count < max_retries:
+            try:
+                await function(*args)
+                return
+            except Exception as e:
+                retry_count += 1
+                print("test")
+                if retry_count > max_retries:
+                    print(f"failed, Exception: {e}")
+                    break
+                await asyncio.sleep(sleep_time * retry_count)
+
+
+class SprzedajemyUtils:
+
+    @staticmethod
+    def get_url_with_localization_input(city: str) -> str:
+        city_formatted = ScraperUtils.replace_polish_characters(city)
+
+        return f"https://sprzedajemy.pl/{city_formatted}"
+
+    @staticmethod
+    def get_amount_of_pages(soup) -> int:
+        x = soup.find("ul", class_="cntPaginator").find_all("a")[4:5]
+        test_soup = BeautifulSoup(str(x[0]), "html.parser")
+        return int(test_soup.find("span").get_text()) - 1
+
+    @staticmethod
+    def get_all_pages_urls(city) -> list[str]:
+        url = SprzedajemyUtils.get_url_with_localization_input(city)
+        pages = [url]
+        soup = ScraperUtils.return_website_string_as_bs4_content(url)
+        for i in range(SprzedajemyUtils.get_amount_of_pages(soup)):
+            cnp = url + f"?offset={30 + i * 30}"
+            pages.append(cnp)
+        return pages
+
+    @staticmethod
+    def find_all_offers_in_selected_page(page: str):
+        offers = []
+        soup = ScraperUtils.return_website_string_as_bs4_content(page)
+        offer_titles = soup.find_all("h2", class_="title")
+        for offer_title in offer_titles:
+            offers.append(f'https://sprzedajemy.pl{offer_title.find("a").get('href')}')
+
+        return offers
+
+    @staticmethod
+    def get_all_offers_urls(pages: list[str]):
+        urls = []
+        for page in tqdm(pages):
+            offers = SprzedajemyUtils.find_all_offers_in_selected_page(page)
+            for offer in offers:
+                urls.append(offer)
+        return urls
+
+    @staticmethod
+    def get_offer_title(soup: BeautifulSoup) -> str:
+        try:
+            return soup.find("span", class_="isUrgentTitle").get_text()
+        except ConnectionError:
+            return "Unknown"
+
+    @staticmethod
+    def get_offer_username(soup: BeautifulSoup) -> str:
+        try:
+            return soup.find("strong", class_="name").get_text()
+        except ConnectionError:
+            return "Username not found"
+
+    @staticmethod
+    def get_offer_location(soup: BeautifulSoup) -> str:
+        try:
+            return soup.find("span", class_="locationName").find("strong").get_text()
+        except ConnectionError:
+            return "Location not found"
+
+    @staticmethod
+    def get_offer_phone_number(soup: BeautifulSoup) -> str:
+        try:
+            phone_number = (
+                    soup.find("span", class_="phone-number-truncated").find("span").get_text()
+                    + " "
+                    + soup.find("span", class_="phone-number-truncated").get("data-phone-end")
+            )
+        except AttributeError:
+            phone_number = "Unknown"
+        except ConnectionError:
+            phone_number = "Unknown"
+        return phone_number
+
+    @staticmethod
+    def get_offer_price(soup: BeautifulSoup) -> str:
+        try:
+            return soup.find("strong", class_="price").find("span").get_text().strip()
+        except AttributeError:
+            return "Unknown"
+        except ConnectionError:
+            return "Unknown"
